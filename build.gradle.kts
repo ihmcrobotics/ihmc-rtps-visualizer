37173944a962b89d0afcb4a54cb7b0a62f494c39
Duncan Calvert <dcalvert@ihmc.us>
2020-12-01 14:20:03 -0600

    :arrow_down: Stay on old version of pub sub

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,12 @@ ihmc {
 app.entrypoint("IHMCRTPSVisualizer", "us.ihmc.rtps.visualizer.IHMCRTPSVisualizer")
 
 mainDependencies {
-   api("us.ihmc:ihmc-pub-sub:0.16.0")
+   api("us.ihmc:ihmc-pub-sub:0.15.0")
    api("commons-io:commons-io:2.8.0")
 }
 
 testDependencies {
-   api("us.ihmc:ros2-common-interfaces:0.20.1")
+   api("us.ihmc:ros2-common-interfaces:0.19.3")
 }
 
 license {
